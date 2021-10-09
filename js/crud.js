9a31ba8aed3d5ed9e00b19976b5be58f6f565de3
Juan Torres <j.sebastiantorres@hotmail.com>
2021-10-09 16:47:10 -0500

    Creando proyecto CRUD

---
+++ js/crud.js
@@ -0,0 +1,20 @@
+//CRUD
+/*
+*Create
+*Read
+*Update
+*Delete
+*/
+
+class CRUD {
+    registros = [1, 2, 3];
+
+    create() => { }
+    read() => {
+        return this.registros;
+    }
+    update() =>{ }
+    delete() =>{ }
+}
+
+let obj = new CRUD();
